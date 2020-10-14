d603cbe9462bc6e4326a494c1496880986147949
sosialajee <sosial.ajee@gmail.com>
2020-10-14 16:25:08 +0800

    New database migration #2

---
+++ database/migrations/2020_10_14_082421_create_spareparts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSparepartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spareparts', function (Blueprint $table) {
+            $table->integer('sparepart_id', true);
+            $table->integer('bengkel_id')->index('bengkel_id_fk');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spareparts');
+    }
+}
