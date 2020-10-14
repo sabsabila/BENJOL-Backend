e3cb8456331f7bd93a0f2c9b46a91cfff3e8eebf
sabsabila <58177825+sabsabila@users.noreply.github.com>
2020-10-14 15:52:55 +0700

    delete duplicated file

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMotorcyclesTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateMotorcyclesTable extends Migration
      */
     public function up()
     {
-        Schema::create('motorcycles', function (Blueprint $table) {
-            $table->integer('motorcycle_id', true);
-            $table->string('brand');
-            $table->string('plate_number');
-            $table->integer('user_id')->index('usermotor_fk_id');
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -28,6 +27,6 @@ class CreateMotorcyclesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('motorcycles');
+        Schema::dropIfExists('password_resets');
     }
 }
