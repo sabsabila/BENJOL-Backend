385cfff56e9dff1bb826c6f5aefa9bd341c7d29f
lina dwi pratiwi <pratiwilinadwi@gmail.com>
2020-10-15 20:00:49 +0700

    CRUD Service Fixes #7

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service;
+use Illuminate\Support\Facades\App;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Service::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $service = new Service;
+        $service->service_name = $request->service_name;
+        $service->cost = $request->cost;
+        if($service->save()){
+            echo "service data successfully added !";
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $service = Service::find($id);
+        $service->service_name = $request->service_name;
+        $service->cost = $request->cost;
+        if($service->save()){
+            echo "service data successfully updated !";
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $service= Service::find($id);
+        // $service->delete();
+        if($service->delete()){
+            echo "service with id" .((int)$id). "has successfully removed.";
+        }
+    }
+}
