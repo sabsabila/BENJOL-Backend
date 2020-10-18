4749e7c62877687abd45e2797f4095ef6d66d0fd
Abdul Salam <abdulsalamsabil@gmail.com>
2020-10-18 20:48:30 +0800

    Relasi Model Payment [Fitur Add & List Payment - Desktop] [Fitur Pay & Show Payment - Mobile] Fixes #27

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    public function booking()
+    {
+        return $this->hasMany('App\Models\Booking');
+    }
+
     protected $primaryKey = 'payment_id';
-    
+
     use HasFactory;
 }
