27c806d91622fd6e9f412a18e68094cd7c20292e
Abdul Salam <abdulsalamsabil@gmail.com>
2020-10-18 20:12:51 +0800

    Relasi Model Payment [Fitur Add & List Payment - Desktop] [Fitur Pay & Show Payment - Mobile] Fixes #27

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('payment', PaymentController::class);
+Route::get('payment', 'PaymentController@index');
+Route::post('payment', 'PaymentController@store');
+Route::get('/payment/{id}', 'PaymentController@show');
+Route::put('/payment/{id}', 'PaymentController@update');
+Route::delete('/payment/{id}', 'PaymentController@destroy');
