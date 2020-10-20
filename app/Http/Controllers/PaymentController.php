6ee881fd3318d9be1fac8290843b611f3ec5388c
sabsabila <58177825+sabsabila@users.noreply.github.com>
2020-10-20 19:29:19 +0700

    restore files

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Payment;
+
+class PaymentController extends Controller
+{
+    public function index()
+    {
+        return Payment::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        $payment = new Payment;
+        $payment->booking_id = $request->booking_id;
+        $payment->total_price = $request->total_price;
+        $payment->receipt = $request->receipt;
+
+        if ($payment->save()) {
+            echo "Data Successfully Added";
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Payment $payment)
+    {
+        return Payment::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Payment $payment)
+    {
+        $payment = Payment::find($id);
+
+        if ($request->booking_id != null)
+            $payment->booking_id = $request->booking_id;
+
+        if ($request->total_price != null)
+            $payment->total_price = $request->total_price;
+
+        if ($request->receipt != null)
+            $payment->receipt = $request->receipt;
+        
+        if ($payment->save()) {
+            echo "Data Successfully Updated";
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Payment $payment)
+    {
+        $payment = Payment::find($id);
+
+        if ($payment->delete()) {
+            echo "Payment with id " . (int) $id . " successfully deleted";
+        }
+    }
+}
---
+++ app/Models/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $primaryKey = 'payment_id';
+    use HasFactory;
+
+    public function booking()
+    {
+        return $this->hasMany('App\Models\Booking');
+    }
+}
